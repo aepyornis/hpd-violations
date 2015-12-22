3feac3bc3857f50e93068299c547f4e081b9f381
aepyornis <ziggy@elephant-bird.net>
2015-12-22 22:47:08 +0000

    converted to command line program

--- parse_violations.py
+++ parse_violations.py
@@ -17,17 +17,22 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import xml.etree.ElementTree as ET
 import csv
+import argparse
+
+parser = argparse.ArgumentParser(description='converts HPD violations data (XML format) into CSVs')
+parser.add_argument('input', help='input xml file')
+parser.add_argument('output', help='output file')
+args = parser.parse_args()
+
+PATH_TO_VIOLATIONS = args.input
+CSV_FILE_NAME = args.output
 
 FIELDS = ['ViolationID', 'BuildingID', 'RegistrationID', 'BoroName', 'Boro', 'HouseNumber', 'LowHouseNumber', 'HighHouseNumber', 'StreetName', 'StreetCode', 'Zip', 'Block', 'Lot', 'Class', 'InspectionDate', 'OriginalCertifyByDate', 'OriginalCorrectByDate', 'NewCertifyByDate', 'NewCorrectByDate', 'CertifiedDate', 'OrderNumber', 'NOVID', 'NOVDescription', 'NOVIssuedDate', 'GroupName', 'SeqNo', 'ShortName', 'LongName', 'Order', 'CurrentStatusDate']
 
-CSV_FILE_NAME = 'violations.csv'
-
-PATH_TO_VIOLATIONS = './violations/ViolationsSept2012.XML'
-
-# write headers
-with open(CSV_FILE_NAME, 'a') as f:
-    writer = csv.DictWriter(f, fieldnames=FIELDS)
-    writer.writeheader()  
+def write_headers():
+    with open(CSV_FILE_NAME, 'a') as f:
+        writer = csv.DictWriter(f, fieldnames=FIELDS)
+        writer.writeheader()  
 
 def parse_one_violation(one_violation):
     root = ET.fromstring(one_violation)
@@ -50,13 +55,19 @@ def parse_one_violation(one_violation):
         writer = csv.DictWriter(f, fieldnames=FIELDS, delimiter='|')
         writer.writerow(violation)
 
-with open ('./violations/ViolationsSept2012.XML', 'r') as f:
-    one_violation = ''
-    for line in f:
-        if line == '<Violation xmlns:i="http://www.w3.org/2001/XMLSchema-instance">\n':
-            one_violation = line
-        elif line == '</Violation>\n':
-            one_violation += line
-            parse_one_violation(one_violation)
-        else:
-            one_violation += line
+def XML_to_csv():
+    with open (PATH_TO_VIOLATIONS, 'r') as f:
+        one_violation = ''
+        for line in f:
+            if line == '<Violation xmlns:i="http://www.w3.org/2001/XMLSchema-instance">\n':
+                one_violation = line
+            elif line == '</Violation>\n':
+                one_violation += line
+                parse_one_violation(one_violation)
+            else:
+                one_violation += line
+
+
+if __name__ == "__main__":
+    write_headers()
+    XML_to_csv()
