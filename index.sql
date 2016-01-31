6c44e1dfe82dc04091f32e4a649090d9ebaa9993
ziggy <ziggy@elephant-bird.net>
2016-01-30 20:44:24 -0500

    indexing

---
+++ index.sql
@@ -0,0 +1,13 @@
+ALTER TABLE  violations ADD COLUMN bbl char(10);
+UPDATE violations SET bbl = boro || lpad(cast(block as text), 5, '0') || lpad(cast(lot as text), 4, '0');
+
+ALTER TABLE violations ADD COLUMN id serial;
+UPDATE violations SET id = DEFAULT;
+ALTER TABLE violations ADD PRIMARY KEY (id);
+
+CREATE INDEX on violations(bbl);
+CREATE INDEX on violations(violationid);
+CREATE INDEX on violations(registrationid);
+CREATE INDEX on violations(violationclass);
+CREATE INDEX on violations(inspectiondate);
+CREATE INDEX on violations(seqno);
