8fb31b3abad7982420023403f48e08ab990a2338
ziggy <ziggy@elephant-bird.net>
2016-01-30 20:11:25 -0500

    update script to copy data into postgres

--- to_postgres.sh
+++ to_postgres.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
+# use: export HPD_VIOLATIONS_DATA_DIR=/path/to/processed/csvs/folder; to_postgres.sh
 
-psql -d violations -f violations_schema.sql 
+psql -d hpd_violations -f violations_schema.sql 
 
-for file in $(ls csvs/); do
+for file in $(ls ${HPD_VIOLATIONS_DATA_DIR}*.csv); do
     printf "copying ${file}\n"
-    windowspath=$(cygpath -aw "csvs/${file}")
-    command="copy violations from '${windowspath}' WITH (FORMAT CSV, HEADER true, NULL '', DELIMITER '|');"
-    echo ${command} | psql violations
+    command="copy violations from '${file}' WITH (FORMAT CSV, HEADER true, NULL '', DELIMITER '|');"
+    echo ${command} | psql hpd_violations
 done
-
-
