0c3fcb9b40f122d0ab5c6c87b5f8c8d3cdf7eb20
ziggy <zig@riseup.net>
2016-11-22 22:56:48 -0500

    Update pg script to conform to nyc-db format

--- to_postgres.sh
+++ to_postgres.sh
@@ -12,50 +12,51 @@
 #                   2016 violation data...
 ############################################
 
-DB="hpd_violations"
-open_violations_file="data/Violation20160731.txt"
+source ./pg_setup.sh
+pwd=$(pwd)
 
 printf "Creating the violations table\n"
-psql -d ${DB} -f sql/schema.sql
+execute_sql sql/schema.sql
 
-cd data/
+cd $HPD_VIOLATIONS_DATA_FOLDER
 for year in 2015 2016; do
     cd $year
     for f in *.txt; do
         printf "copying "${year}"/"${f}"\n"
         cat ${f} |
             sed 's/"//g' |   # DoubleQuotes just mess things up
-            psql -d ${DB} -c "COPY violations from STDIN (DELIMITER '|', FORMAT CSV, HEADER TRUE);"
+            execute_sql_cmd "COPY violations from STDIN (DELIMITER '|', FORMAT CSV, HEADER TRUE);"
     done
     cd ..
 done
-cd ..
+
+cd $pwd
 
 printf "Adding the bbl lookup table\n"
-psql -d ${DB} -f sql/bbl_lookup.sql 
+execute_sql sql/bbl_lookup.sql 
 
-psql -d ${DB} -c "COPY bbl_lookup from '"$(readlink -f data/bbl_lat_lng.txt)"' (FORMAT CSV,  HEADER TRUE)"
+cat ${BBL_LAT_LNG_FILE} | execute_sql_cmd "COPY bbl_lookup from STDIN (FORMAT CSV,  HEADER TRUE)"
 
 printf "Adding columns -- bbl, lat, lng -- to violations\n"
-psql -d ${DB} -f sql/process_violations_table.sql
+execute_sql sql/process_violations_table.sql
 
 printf "Creating the uniq_violations table\n"
-psql -d ${DB} -f sql/unique_violations.sql 
+execute_sql sql/unique_violations.sql 
 
 printf "Creating the open_violations table\n"
-psql -d ${DB} -f sql/open_violations_schema.sql
+execute_sql sql/open_violations_schema.sql
 
-cat ${open_violations_file} |
+cat $HPD_OPEN_VIOLATIONS_FILE |
     sed 's/"//g' |
-    psql -d ${DB} -c "COPY open_violations from STDIN (DELIMITER '|', FORMAT CSV, HEADER TRUE);"
+    execute_sql_cmd "COPY open_violations from STDIN (DELIMITER '|', FORMAT CSV, HEADER TRUE);"
 
 printf "Adding columns -- bbl, lat, lng -- to open_violations\n"
-psql -d ${DB} -f sql/process_open_violations.sql
+execute_sql sql/process_open_violations.sql
 
 printf "Dropping bbl lookup table\n"
-psql -d ${DB} -c "DROP TABLE bbl_lookup;"
+execute_sql_cmd "DROP TABLE bbl_lookup;"
 
 printf "Creating the all_violations table\n"
-psql -d ${DB} -f sql/all_violations.sql
+execute_sql sql/all_violations.sql
 
-printf "There are "$(psql -At -d hpd_violations -c "SELECT COUNT(*) from all_violations")" rows in the all_violations table\n"
+# printf "There are "$(psql -At -d hpd_violations -c "SELECT COUNT(*) from all_violations")" rows in the all_violations table\n"
